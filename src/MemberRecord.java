c37df2dcf52927d0250dc2d0092ba553f8de5966
Ethan Nguyen <ehnguyen@crimson.ua.edu>
2023-04-12 05:27:47 +0100

    Added my classes as well as edited the class diagram

---
+++ src/MemberRecord.java
@@ -0,0 +1,5 @@
+class MemberRecord extends Record {
+    public MemberRecord(String name, String number, String address, String city, String state, String zipCode){
+        super(name, number, address, city, state, zipCode);
+    }
+}
---
+++ src/ProviderRecord.java
@@ -0,0 +1,5 @@
+class ProviderRecord extends Record {
+    public ProviderRecord(String name, String number, String address, String city, String state, String zipCode){
+        super(name, number, address, city, state, zipCode);
+    }
+}
