551304211a077e0d4a7cd517710e56089bd42849
Austin Smith <alsmith77@crimson.ua.edu>
2023-04-13 18:45:59 -0500

    Updated verification class, pending use of displayInput();

--- src/Verification.java
+++ src/Verification.java
@@ -1,29 +1,37 @@
+import java.util.Scanner; //Allows use of user input
 
 public class Verification{
 
-
+    //Variables
     int userNum;
-
     Credentials credentials;
+    Scanner userReader = new Scanner(System.in);
 
     String displayInput(){
         return null;
     
     }
-    void enterInformation(){
 
+    //Prompts for user number
+    void enterInformation(){
+        System.out.println("Enter Number:\n");
+            userNum = userReader.nextInt();
     }
+
+    /* Will return value based on role of user ie. manager, provider,
+     or operator. -1 indicates user is not valid.*/
+
     int userVerification(){
         if(credentials.searchManagerList(userNum) == true){
-            return 0;
+            return 0; //is manager
         }
         if(credentials.searchProviderList(userNum) == true){
-            return 1;
+            return 1; //is provider
         }
         if(credentials.searchOperatorList(userNum) == true){
-            return 2;
+            return 2; //is opperator
         }
-        else return -1;
+        else return -1; //unauthorized user
 
         
     }
