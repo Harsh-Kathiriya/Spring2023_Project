295f355183bbf53ed78aaaac59eab0c1ee87995a
Chase_Fetherling <cmfetherling@crimson.ua.edu>
2023-04-24 21:05:04 -0500

    Started the ServiceList file

---
+++ src/ServiceList.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+public class ServiceList {
+    public ServiceList() {
+        try {
+            File directoryInfo = new File("C:\\Users\\Chase\\Visual Studio Programs\\testText.txt");
+            Scanner readIn = new Scanner(directoryInfo);
+        }
+    }
+   
+   
+
+
+
+
+}
