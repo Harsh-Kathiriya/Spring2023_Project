531597c8d9a3ad4b39053f54b493c0f9b4979bb8
Sebastian Shirk <srshirk@crimson.ua.edu>
2023-04-26 14:51:50 -0500

    a

--- src/ManagerController.java
+++ src/ManagerController.java
@@ -1,18 +1,17 @@
 public class ManagerController {
 
-  public static void /* not sure if should be void or somwthing else? */ RequestProviderReport(){
-    ProviderReport providerReport = new ProviderReport(/* ? */);
-    System.out.println(providerReport); /* Temporary till we have the menus to display to */
+  public static void RequestProviderReport() {
+    ProviderReport providerReport = new ProviderReport();
+    System.out.println(providerReport);
   }
 
-  public static void /* not sure if should be void or somwthing else? */ RequestSummaryReport(){
-    SummaryReport summaryReport = new SummaryReport(/* ? */);
-    System.out.println(summaryReport); /* Temporary till we have the menus to display to */
+  public static void RequestSummaryReport() {
+    SummaryReport summaryReport = new SummaryReport();
+    System.out.println(summaryReport);
   }
-  // Don't think i can do much more until we have actual files for me to pull from or menus for me to display to //
 
-  public static void RequestMemberReport(){
+  public static void RequestMemberReport() {
     MemberReport memberReport = new MemberReport();
-    System.out.println(memberReport); 
+    System.out.println(memberReport);
   }
 }
