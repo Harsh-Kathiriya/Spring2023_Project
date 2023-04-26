33ce721eb73d1c2731fbaeda15a4fac224e7be2f
ecurl3 <ecurl3@crimson.ua.edu>
2023-04-26 16:08:00 -0500

    record stuff

--- src/providerRecordHash.java
+++ src/providerRecordHash.java
@@ -1,3 +1,19 @@
 public class providerRecordHash {
-    
+
+    class Provider{
+        private String name;
+        private String id;
+        private String adress;
+        private String state;
+        private String zip;
+
+
+    }
+
+
+    providerRecordHash(){
+        
+
+
+    }
 }
