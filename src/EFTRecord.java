cf351edb24a90209e8db4bc4f90cb22a9545edf7
Chase_Fetherling <cmfetherling@crimson.ua.edu>
2023-04-25 22:29:34 -0500

    Added EFTRecord implementation

--- src/EFTRecord.java
+++ src/EFTRecord.java
@@ -1,13 +1,53 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+
 
 class EFTRecord {
     // initializing fields
-    private double totalFee;
-
+    private String EFTLocation;
+    private File directoryInfo;
+    private String providerName;
+    private int providerNumber;
+    private int transferAmount;
     // default constructor does not intialize any values
     EFTRecord() {
 
     }
 
     // Constructor that provides all fields of the report
+    public EFTRecord(String providerName, int providerNumber, int transferAmount) {
+        this.providerName = providerName;
+        this.providerNumber = providerNumber;
+        this.transferAmount = transferAmount;
+        try {
+            String userDirectory = System.getProperty("user.dir");
+            EFTLocation = userDirectory + "/spring23team1/src/" + providerName + ".txt";
+            directoryInfo = new File(EFTLocation);
+            FileWriter EFTWriter = new FileWriter(EFTLocation);
+            EFTWriter.write(providerName + " " + Integer.toString(providerNumber) +  " " + Integer.toString(transferAmount));
+            EFTWriter.close();
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("FileError");
+        }
+        catch(IOException e) {
+            System.out.println("IOError");
+        }
+    }
+
+    public String getProviderName() {
+        return providerName;
+    }
+
+    public int getProviderNumber() {
+        return providerNumber;
+    }
+
+    public int getTransferAmount() {
+        return transferAmount;
+    }
+
     
 }
