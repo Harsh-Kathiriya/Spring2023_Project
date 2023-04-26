200db22545fabaaa0a141a9cd191542e20c72472
Austin Smith <alsmith77@crimson.ua.edu>
2023-04-26 15:52:32 -0500

    update to member hash

--- src/MemberRecordHash.java
+++ src/MemberRecordHash.java
@@ -1,3 +1,18 @@
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+
+
 public class MemberRecordHash {
-    
+
+    File memberFile= new File(System.getProperty("user.dir") + "/Member_Record");
+            Scanner memberReader = new Scanner(memberFile);
+
+            while(memberReader.hasNextLine()) {
+                String data = memberReader.nextLine();
+                String[] temp = data.split(",");
+                
+            }
+            memberReader.close();
+
 }
