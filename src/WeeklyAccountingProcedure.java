85f472bd488e55a7b00a2b0440888f229e0dc3fa
Austin Smith <alsmith77@crimson.ua.edu>
2023-04-26 14:07:20 -0500

    methods added in WeeklyAccountingProcedure

--- src/WeeklyAccountingProcedure.java
+++ src/WeeklyAccountingProcedure.java
@@ -1,3 +1,20 @@
+
 public class WeeklyAccountingProcedure {
-    
+    void requestProviderReport(){
+        
+    }
+
+    void requestMemberReport(){
+
+    }
+
+    void requestSummaryReport(){
+
+    }
+
+    void requestETFReport(){
+
+    }
+
+
 }
