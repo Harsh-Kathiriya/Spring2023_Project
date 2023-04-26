695099d7ff0ff42e6b03566801d35d68561024a8
Austin Smith <alsmith77@crimson.ua.edu>
2023-04-26 16:05:44 -0500

    Update MemberRecordHash

--- src/MemberRecordHash.java
+++ src/MemberRecordHash.java
@@ -5,14 +5,14 @@ import java.util.Scanner; // Import the Scanner class to read text files
 
 public class MemberRecordHash {
 
-    File memberFile= new File(System.getProperty("user.dir") + "/Member_Record");
-            Scanner memberReader = new Scanner(memberFile);
+    class member{
+        String number;
+        String memberName;
+        String street;
+        String city;
+        String state;
+        String zipCode;
+    }
 
-            while(memberReader.hasNextLine()) {
-                String data = memberReader.nextLine();
-                String[] temp = data.split(",");
-                
-            }
-            memberReader.close();
-
+    
 }
