c387aa544d93a232d5afefca4ea38ac851d6942a
ecurl3 <ecurl3@crimson.ua.edu>
2023-04-26 22:31:36 -0500

    hash tables fixed

--- src/MemberRecordHash.java
+++ src/MemberRecordHash.java
@@ -1,16 +1,51 @@
 import java.io.File;  // Import the File class
 import java.util.Scanner;
 import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.io.IOException;
 import java.util.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 
 public class MemberRecordHash {
+    private static final String MEMBER_FILE_NAME = System.getProperty("user.dir") + "/src/Provider_Record";
+    public static String memberID = "258369258";
 
     List<Integer> sortedMap;
     Hashtable<Integer, MemberRecord> info;
 
-    MemberRecordHash(){
+    MemberRecordHash() throws IOException{
         info = new Hashtable<>();
+        List<String> lines = Files.readAllLines(Paths.get(MEMBER_FILE_NAME));
+        for(String line : lines){
+            if(line.startsWith(memberID+",")){
+                String[] parts = line.split(",");
+                String name = parts[0];
+                String id = parts[1];
+                String adress = parts[3];
+                String city = parts[4];
+                String state = parts[5];
+                String zip = parts[6];
+                
+                MemberRecord member = new MemberRecord(name, id, adress, city, state, zip);
+                int num = Integer.parseInt(id);
+                info.put(num, member);
+            }
+        }
+        
+
+        
+        
+        /* 
         File recordFile = new File("C:\\Users\\emily\\OneDrive\\Desktop\\cs200 proj\\projectRepository\\spring23team1\\src\\Member_Record.txt");
 
         try{
@@ -36,12 +71,13 @@ public class MemberRecordHash {
             System.out.println("file not found");
             e.printStackTrace();
         }
+        */
 
         sortedMap = new ArrayList<>(info.keySet());
         Collections.sort(sortedMap);
 
     }
-    
+
     public MemberRecord memberAt(Integer ident){
         MemberRecord person = info.get(ident);
         return person;
--- src/providerRecordHash.java
+++ src/providerRecordHash.java
@@ -2,17 +2,47 @@ import java.io.File;  // Import the File class
 import java.util.Scanner;
 import java.io.FileNotFoundException;  // Import this class to handle errors
 import java.util.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 
 public class providerRecordHash {
 
+    private static final String PROVIDER_FILE_NAME = System.getProperty("user.dir") + "/src/Provider_Record";
+    public static String providerID = "123456789";
     List<Integer> sortedMap;
     Hashtable<Integer, ProviderRecord> info;
 
-    providerRecordHash(){
+    providerRecordHash() throws IOException{
         info = new Hashtable<>();
-        File recordFile = new File("C:\\Users\\emily\\OneDrive\\Desktop\\cs200 proj\\projectRepository\\spring23team1\\src\\Provider_Record.txt");
+        List<String> lines = Files.readAllLines(Paths.get(PROVIDER_FILE_NAME));
+        for(String line : lines){
+            if(line.startsWith(providerID+",")){
+                String[] parts = line.split(",");
+                String name = parts[0];
+                String id = parts[1];
+                String adress = parts[3];
+                String city = parts[4];
+                String state = parts[5];
+                String zip = parts[6];
+                
+                ProviderRecord provider = new ProviderRecord(name, id, adress, city, state, zip);
+                int num = Integer.parseInt(id);
+                info.put(num, provider);
+            }
+        }
 
+        /* 
         try{
             Scanner recordReader = new Scanner(recordFile);
             while(recordReader.hasNextLine()) {
@@ -36,9 +66,11 @@ public class providerRecordHash {
             System.out.println("file not found");
             e.printStackTrace();
         }
+        */
 
         sortedMap = new ArrayList<>(info.keySet());
         Collections.sort(sortedMap);
+
 
     }
 
