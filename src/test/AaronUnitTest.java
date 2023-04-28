640468805675fe9a1b9f02af073ca9c7c917daea
aabuff <aabuff@crimson.ua.edu>
2023-04-28 00:00:47 -0500

    Moved AaronUnitTest

--- src/test/AaronUnitTest.java
+++ src/test/AaronUnitTest.java
@@ -1,16 +1,11 @@
-package test;
 import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-import classes.ProviderDirectory;
-
 
 public class AaronUnitTest{
 
-
     ProviderDirectory testDirect = new ProviderDirectory();
-
 
     @Before
     public void SetUp() throws Exception{
@@ -19,30 +14,23 @@ public class AaronUnitTest{
         testDirect.serviceFees.add(30);
     }
 
-
     @Test
     public void feeLookupSuccesstest(){
         assertTrue(testDirect.feeLookup(123456) == 30);
     }
-
 
     @Test
     public void feeLookupFailureTest(){
         assertFalse(testDirect.feeLookup(123456) == 10);
     }
 
-
     @Test
     public void serviceLookupSuccessTest(){
         assertTrue(testDirect.serviceLookup(123456) == "Test Service");
     }
-
 
     @Test
     public void serviceLookupFailureTest(){
         assertFalse(testDirect.serviceLookup(123456) == "Bad Service");
     }
 }
-
-
-
