819a3cb65f17037e55ab032c2edeec4907357f7a
Sebastian Shirk01~ <srshirk@crimson.ua.edu>
2023-04-27 22:56:44 -0500

    p

--- src/SebastianUnitTest.java
+++ src/SebastianUnitTest.java
@@ -9,6 +9,7 @@ public class SebastianUnitTest {
 
     private ManagerController managerController;
     private ProviderRecord providerRecord;
+    private MemberRecord memberRecord;
 
     @Before
     public void setUp() throws Exception {
@@ -28,7 +29,7 @@ public class SebastianUnitTest {
 
     @Test
     public void FailureTestRequestSingleProviderReport() {
-        ProviderRecord providerRecord = new ProviderRecord("John Smith", "100000001", "123 Main St", "Anytown", "NY", "12345");
+        ProviderRecord providerRecord = new ProviderRecord("John Smith", "1000001989989", "123 Main St", "Anytown", "NY", "12345");
         ProviderReport providerReport = managerController.requestSingleProviderReport(providerRecord);
         assertNotNull(providerReport);
         assertEquals(providerRecord.getName(), providerReport.getProviderName());
@@ -46,7 +47,7 @@ public class SebastianUnitTest {
 
     @Test
     public void FailureTestRequestSingleMemberReport() {
-        MemberRecord memberRecord = new MemberRecord("Alice Johnson", "200000001", "456 High St", "Anytown", "NY", "12345");
+        MemberRecord memberRecord = new MemberRecord("Alice Johnson", "200000198987", "456 High St", "Anytown", "NY", "12345");
         MemberReport memberReport = managerController.requestSingleMemberReport(memberRecord);
         assertNotNull(memberReport);
         assertEquals(memberRecord.getName(), memberReport.getMemberName());
@@ -63,7 +64,7 @@ public class SebastianUnitTest {
         assertEquals("12345", providerRecord.getZipCode());
     }
 
-    @Test
+        @Test
     public void FailureProvideRecord() {
         providerRecord.setNumber("12345678999999999");
         assertEquals("Ethan", providerRecord.getName());
