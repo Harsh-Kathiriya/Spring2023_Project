7724006a029cdb6a55a2f7077f1f0f859fc62537
Sebastian Shirk01~ <srshirk@crimson.ua.edu>
2023-04-27 22:46:25 -0500

    pushing

--- src/SebastianUnitTest.java
+++ src/SebastianUnitTest.java
@@ -25,7 +25,7 @@ public class SebastianUnitTest {
 
     @Test
     public void FailureTestRequestSingleProviderReport() {
-        ProviderRecord providerRecord = new ProviderRecord("John Smith", "100000001989989", "123 Main St", "Anytown", "NY", "12345");
+        ProviderRecord providerRecord = new ProviderRecord("John Smith", "1000001989989", "123 Main St", "Anytown", "NY", "12345");
         ProviderReport providerReport = managerController.requestSingleProviderReport(providerRecord);
         assertNotNull(providerReport);
         assertEquals(providerRecord.getName(), providerReport.getProviderName());
@@ -38,16 +38,16 @@ public class SebastianUnitTest {
         MemberReport memberReport = managerController.requestSingleMemberReport(memberRecord);
         assertNotNull(memberReport);
         assertEquals(memberRecord.getName(), memberReport.getMemberName());
-        assertEquals(memberRecord.getNumber(), memberReport.getMemberNumber());
+        assertNotEquals(memberRecord.getNumber(), memberReport.getMemberNumber());
     }
 
     @Test
     public void FailureTestRequestSingleMemberReport() {
-        MemberRecord memberRecord = new MemberRecord("Alice Johnson", "20000000198987", "456 High St", "Anytown", "NY", "12345");
+        MemberRecord memberRecord = new MemberRecord("Alice Johnson", "200000198987", "456 High St", "Anytown", "NY", "12345");
         MemberReport memberReport = managerController.requestSingleMemberReport(memberRecord);
         assertNotNull(memberReport);
         assertEquals(memberRecord.getName(), memberReport.getMemberName());
-        assertEquals(memberRecord.getNumber(), memberReport.getMemberNumber());
+        assertNotEquals(memberRecord.getNumber(), memberReport.getMemberNumber());
     }
 
 }
